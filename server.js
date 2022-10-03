5d766a4f64f02b590e752dca9401bacbdafad10b
BT616 <110855674+BT616@users.noreply.github.com>
2022-10-03 16:02:21 -0700

    trying to get.sql in order

--- server.js
+++ server.js
@@ -1,4 +1,43 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const cTable = require('console.table');
+const mysql = require('mysql2');
+
+//1
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user:'root',
+    password:"", // process.env.MYSQL_PASSWORD
+    database:"company_db"
+},
+console.log('Connect to the the company_db database')
+);
+//2
+connection.connect(err =>{
+    if(err) throw err;
+    afterConnection()
+});
+//3
+afterConnection =()=>{
+    promptUser();
+}
+
+
+
+
+//then . 4 
+const promptUser = ()=> {
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'choices',
+        message: "What would you like to do",
+        choices: ["view departments?", "view roles?", "view all employess",""
+    ]
+    }
+
+])
+
+}
+
 
